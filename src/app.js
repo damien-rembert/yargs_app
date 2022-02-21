31094fd9fac94a6c44338acd9a025d10992f6592
Damien R <damien.rembert@gmail.com>
2022-02-21 16:01:56 +0000

    adding multiple films

--- src/app.js
+++ src/app.js
@@ -5,14 +5,43 @@ const Movie = require("./utils");
 // yargs allows you to call this app like this:
 // node app.js --add --title "Spiderman" --actor "Tom Holland"
 
-const app = (yargsObj) => {
-    if (yargsObj.add) {
-        const movie = new Movie();
-        movie.add();
-        console.log(movie.list());
-    } else {
-        console.log("Incorrect command");
-    }
-};
+// const app = (yargsObj) => {
+//     if (yargsObj.add) {
+//         const movie = new Movie();
+//         movie.add(this.title);
+//         console.log(movie.list());
+//     // } else if (yargsObj.addMulti) {
+//     //     let titles = 
+//     //     for (let i = 0; i < movie.addMulti; i++) {
+
+//     //         const element = array[i];
+            
+//     //     }
+
+
+//     //     const movie = new Movie();
+//     //     movie.add();
+//     //     console.log(movie.list());
+//     } else {
+//         console.log("Incorrect command");
+//     }
+// };
+
+    const app = (yargsObj) => {
+        if (yargsObj.add) {
+            const movie = new Movie(yargsObj.title, yargsObj.actor);
+            movie.add();
+            console.log(movie.list());
+            // add movie to array, consol log at end of runtime
+        } else if (yargsObj.addMore) {
+                const movie = new Movie(yargsObj.title, yargsObj.actor);
+                const movie1 = new Movie(yargsObj.title1, yargsObj.actor1);
+                movie.add();
+                movie1.add();
+                console.log(movie1.list());
+            } else {
+                console.log("Incorrect Command")
+            }
+    };
 
 app(yargs.argv);
