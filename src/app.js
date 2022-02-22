e5257c995c30708335823370468c92777df9f911
Damien R <damien.rembert@gmail.com>
2022-02-22 10:20:31 +0000

    adding multiple films with one call

--- src/app.js
+++ src/app.js
@@ -1,47 +1,52 @@
 const yargs = require("yargs");
 const Movie = require("./utils");
 
-// console.log(yargs.argvs);
-// yargs allows you to call this app like this:
-// node app.js --add --title "Spiderman" --actor "Tom Holland"
-
-// const app = (yargsObj) => {
-//     if (yargsObj.add) {
-//         const movie = new Movie();
-//         movie.add(this.title);
-//         console.log(movie.list());
-//     // } else if (yargsObj.addMulti) {
-//     //     let titles = 
-//     //     for (let i = 0; i < movie.addMulti; i++) {
-
-//     //         const element = array[i];
-            
-//     //     }
-
-
-//     //     const movie = new Movie();
-//     //     movie.add();
-//     //     console.log(movie.list());
-//     } else {
-//         console.log("Incorrect command");
-//     }
-// };
-
     const app = (yargsObj) => {
         if (yargsObj.add) {
             const movie = new Movie(yargsObj.title, yargsObj.actor);
             movie.add();
-            console.log(movie.list());
+            // console.log(movie.list());
             // add movie to array, consol log at end of runtime
         } else if (yargsObj.addMore) {
+            let movieArr = [];
+            if (typeof yargsObj.title !== "string") {
+                // actor  for every title
+                if (yargsObj.actor && yargsObj.title.length == yargsObj.actor.length) {
+                    for (let i = 0; i < yargsObj.title.length; i++) {
+                        const movie = new Movie(yargsObj.title[i], yargsObj.actor[i]);
+                        // movie.add();
+                        movieArr.push(movie);
+                    }
+                // just title    
+                } else {
+                    for (let i = 0; i < yargsObj.title.length; i++) {
+                        const movie = new Movie(yargsObj.title[i]);
+                        // movie.add();
+                        movieArr.push(movie);
+                    }
+                }
+        } else {
+            if (yargsObj.actor) {
                 const movie = new Movie(yargsObj.title, yargsObj.actor);
-                const movie1 = new Movie(yargsObj.title1, yargsObj.actor1);
-                movie.add();
-                movie1.add();
-                console.log(movie1.list());
+                movieArr.push(movie);
             } else {
-                console.log("Incorrect Command")
+                const movie = new Movie(yargsObj.title);
+                movieArr.push(movie);
             }
+    
+
+        }
+        console.log(movieArr);
+
+                // const movie = new Movie(yargsObj.title, yargsObj.actor);
+                // const movie1 = new Movie(yargsObj.title1, yargsObj.actor1);
+                // movie.add();
+                // movie1.add();
+                // console.log(movie1.list());
+        } else {
+            console.log("Incorrect Command")
+        }
     };
 
+// console.log(yargs.argv);
 app(yargs.argv);
